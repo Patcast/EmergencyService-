a1fe719c57ad692208634b5f91018f3a9c9aafda
patricioadolfo.castillocalderon <you@example.com>
2022-10-26 09:40:05 +0200

    first commit

---
+++ codeEmergencyCenter/emergencycenter.h
@@ -0,0 +1,12 @@
+#ifndef EMERGENCYCENTER_H
+#define EMERGENCYCENTER_H
+
+#include "EmergencyCenter_global.h"
+
+class EMERGENCYCENTER_EXPORT EmergencyCenter
+{
+public:
+    EmergencyCenter();
+};
+
+#endif // EMERGENCYCENTER_H
