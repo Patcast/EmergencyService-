379d52c5fc847092c81008bb62107e4e8f79d9d1
thomasw <thomas.wens@student.kuleuven.be>
2022-10-30 20:28:12 +0100

    smartpointer to be implemented

--- codeEmergencyCenter/component.h
+++ codeEmergencyCenter/component.h
@@ -2,13 +2,11 @@
 #define COMPONENT_H
 #include <string>
 #include <QVector>
-#include "sensor.h"
+
 
 
 class component
 {
-
-
 private:
     int Id;
     std::string description;
@@ -18,11 +16,10 @@ public:
     int getId();
     std::string getDescription();
     void addNewComponent(component *c);
-    void addNewSensor(Sensor *c);
     void remove(component *c);
-    QVector<component*> getChildren();
-    void activateSensor();
-
+    virtual int activateSensor();
+    virtual int deactivateSensor();
+    virtual int testSensor();
 };
 
 #endif // COMPONENT_H
